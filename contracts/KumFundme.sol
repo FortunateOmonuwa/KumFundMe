bbdd4c2e17321acff61b2f49848c55ecf11f1a08
FortunateOmonuwa <omonuwafortunate@gmail.com>
2025-05-21 16:01:26 +0100

    Initial commit

---
+++ contracts/KumFundme.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "contracts/PriceConverter.sol";
+contract KumFundme{
+    AggregatorV3Interface priceFeed;
+    using PriceConverter for uint256;
+
+    address private owner;
+    struct Donor{
+        address donor;
+        string message;
+        uint256 value;
+    }
+
+    Donor[] public donors;
+    mapping (address sender => uint256 value) public senderToValue;
+    mapping (address sender => Donor ) public addressToDonor;
+    uint256 public minimumVal = 10e18;
+
+    constructor(){
+        owner = msg.sender;
+        priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    }
+
+    function donate() public payable returns (string memory){
+        uint256 ethSent = msg.value.getConversionRate(priceFeed);
+        require(ethSent >= minimumVal, "Eth donated can't be less than $10");
+        senderToValue[msg.sender] += ethSent;
+
+        Donor memory newDonor = Donor({
+            donor : msg.sender,
+            message: "Thank you for donating",
+            value : msg.value
+        });
+        addressToDonor[msg.sender] =  newDonor;
+        donors.push(newDonor);
+        return "Transaction Successful";
+    }
+
+    function getAmountDonated(address donor) public view returns (uint256){
+        uint256 amount = addressToDonor[donor].value;
+        return amount;
+    }
+
+    function withdrawAllFunds() public onlyOwner{
+        (bool success,) = payable (msg.sender).call{value: address(this).balance}("");
+        require(success, "Withdraw operation failed");
+    }
+
+    receive() external payable {
+        donate();
+     }
+    fallback() external payable {
+        donate();
+    }
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+}
